d3c78964bd7248f4048e4926617b809f00b3b211
aab-dii <mabdillahabdi600@gmail.com>
2024-05-31 20:18:17 +0800

    Update: Database

--- Database/dbtoko.sql
+++ Database/dbtoko.sql
@@ -28,7 +28,7 @@ CREATE TABLE `tbkeranjang` (
   `idProduk` int NOT NULL,
   `jumlah` int NOT NULL,
   PRIMARY KEY (`idKeranjang`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,6 @@ CREATE TABLE `tbkeranjang` (
 
 LOCK TABLES `tbkeranjang` WRITE;
 /*!40000 ALTER TABLE `tbkeranjang` DISABLE KEYS */;
-INSERT INTO `tbkeranjang` VALUES (10,8,11,10),(11,8,11,5),(12,8,11,20);
 /*!40000 ALTER TABLE `tbkeranjang` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +54,7 @@ CREATE TABLE `tbpesanan` (
   `jumlah` int NOT NULL,
   `status` varchar(100) NOT NULL,
   PRIMARY KEY (`idPesanan`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +63,7 @@ CREATE TABLE `tbpesanan` (
 
 LOCK TABLES `tbpesanan` WRITE;
 /*!40000 ALTER TABLE `tbpesanan` DISABLE KEYS */;
-INSERT INTO `tbpesanan` VALUES (1,8,11,20,'Pesanan Diproses');
+INSERT INTO `tbpesanan` VALUES (53,8,14,123,'Pesanan Sedang Dikirim'),(54,8,14,20,'Menunggu Konfirmasi'),(55,8,14,20,'Menunggu Konfirmasi'),(56,8,14,30,'Menunggu Konfirmasi'),(57,8,14,20,'Menunggu Konfirmasi'),(58,8,14,30,'Menunggu Konfirmasi'),(59,8,14,20,'Menunggu Konfirmasi'),(60,8,14,20,'Menunggu Konfirmasi'),(61,8,14,20,'Menunggu Konfirmasi'),(62,8,14,10,'Pesanan Diproses'),(63,8,14,10,'Menunggu Konfirmasi'),(64,8,14,10,'Menunggu Konfirmasi'),(65,8,14,10,'Menunggu Konfirmasi'),(66,14,14,1,'Menunggu Konfirmasi'),(67,14,14,1,'Menunggu Konfirmasi');
 /*!40000 ALTER TABLE `tbpesanan` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,8 +82,10 @@ CREATE TABLE `tbproduk` (
   `deskripsi` text,
   `merk` varchar(100) DEFAULT NULL,
   `jenis` varchar(100) NOT NULL,
+  `bahan` varchar(100) DEFAULT NULL,
+  `ukuran` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +94,7 @@ CREATE TABLE `tbproduk` (
 
 LOCK TABLES `tbproduk` WRITE;
 /*!40000 ALTER TABLE `tbproduk` DISABLE KEYS */;
-INSERT INTO `tbproduk` VALUES (2,'k1',1,1.00,'k1','1','perkakas'),(4,'f1',1,1.00,'f1','1','furniture'),(11,'234',234,234.00,'234','234','rumahtangga'),(13,'13',123,123.00,'123','123','rumahtangga');
+INSERT INTO `tbproduk` VALUES (2,'k1',1,1.00,'k1','1','perkakas',NULL,NULL),(4,'f1',1,1.00,'f1','1','furniture',NULL,NULL),(14,'123',91,123.00,'123','123','rumahtangga',NULL,NULL),(16,'qwer',132,123.00,'qwer','123','perkakas',NULL,NULL),(17,'rty',123,123.00,'132','123','elektronik',NULL,NULL),(18,'ghj',123,123.00,'123','123','furniture',NULL,NULL),(19,' ',1,1.00,' ',' ','rumahtangga',NULL,NULL),(20,' ',22,1.00,'1 ','2','rumahtangga',NULL,NULL),(21,'1',1,1.00,'1','1','rumahtangga',NULL,NULL);
 /*!40000 ALTER TABLE `tbproduk` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,11 +111,12 @@ CREATE TABLE `tbuser` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `telp` bigint DEFAULT NULL,
+  `telp` varchar(100) DEFAULT NULL,
   `alamat` text,
   `role` varchar(100) NOT NULL,
+  `saldo` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,7 +125,7 @@ CREATE TABLE `tbuser` (
 
 LOCK TABLES `tbuser` WRITE;
 /*!40000 ALTER TABLE `tbuser` DISABLE KEYS */;
-INSERT INTO `tbuser` VALUES (5,'admin','admin','admin123','admin@gmail.com',812312345,NULL,'admin'),(6,NULL,'123','8023fd0ad4203dfc402481e651de11392c84c51f5336de1d56a9c1dffef732b1',NULL,NULL,NULL,'customer'),(7,NULL,'abdi','8023fd0ad4203dfc402481e651de11392c84c51f5336de1d56a9c1dffef732b1',NULL,NULL,NULL,'customer'),(8,NULL,'abdul','123',NULL,NULL,NULL,'customer'),(9,NULL,'aku','123',NULL,NULL,NULL,'customer'),(11,NULL,'tes','123',NULL,NULL,NULL,'kurir'),(12,'abdi','tes2','123','abdi@gmail.com',123,NULL,'kurir');
+INSERT INTO `tbuser` VALUES (5,'admin','admin','admin123','admin@gmail.com','812312345',NULL,'admin',NULL),(6,NULL,'123','8023fd0ad4203dfc402481e651de11392c84c51f5336de1d56a9c1dffef732b1',NULL,NULL,NULL,'customer',NULL),(7,NULL,'abdi','8023fd0ad4203dfc402481e651de11392c84c51f5336de1d56a9c1dffef732b1',NULL,NULL,NULL,'customer',NULL),(8,'abdi','abdul','123','abdi@gmail.com','123','jl, amsangaji','customer',125000),(9,NULL,'aku','123',NULL,NULL,NULL,'customer',NULL),(12,'abdi','tes2','123','abdi@gmail.com','123',NULL,'kurir',NULL),(13,'123','qwe','qwe','kurir@gmail.com','123',NULL,'kurir',NULL),(14,'abdel','abdel','123','a@gmail.com','213','123','customer',100000);
 /*!40000 ALTER TABLE `tbuser` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -136,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-29 22:39:41
+-- Dump completed on 2024-05-31 20:17:21
